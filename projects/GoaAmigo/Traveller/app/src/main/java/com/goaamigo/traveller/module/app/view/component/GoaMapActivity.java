27358fd1c44cfb68839c98a98d3479742a547679
Gurinder <gurinderjeet30@gmail.com>
2015-11-05 16:54:02 +0530

    deleted build

--- projects/GoaAmigo/Traveller/app/src/main/java/com/goaamigo/traveller/module/app/view/component/GoaMapActivity.java
+++ projects/GoaAmigo/Traveller/app/src/main/java/com/goaamigo/traveller/module/app/view/component/GoaMapActivity.java
@@ -2,6 +2,8 @@ package com.goaamigo.traveller.module.app.view.component;
 
 import android.os.Bundle;
 
+import com.goaamigo.traveller.R;
+
 import org.mapsforge.core.model.LatLong;
 import org.mapsforge.core.model.MapPosition;
 import org.mapsforge.map.android.graphics.AndroidGraphicFactory;
